f186defa11ef41fc7f8da81bd41a48b13b39992d
Ridar Gustia <ridargp@gmail.com>
2020-11-13 21:30:33 +0700

    Menambahkan url '../v1/..' di routing API

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Http\Request;
 
-Route::post('auth/register', 'AuthController@register');    //Fitur API untuk register
-Route::post('auth/login', 'AuthController@login');  //Fitur API untuk Login
-Route::get('user/profile', 'UserController@profile')->middleware('auth:api');
-Route::post('place', 'PlaceController@add')->middleware('auth:api');
-Route::get('place', 'PlaceController@show')->middleware('auth:api');
+Route::post('v1/auth/register', 'AuthController@register');    //Fitur API untuk register
+Route::post('v1/auth/login', 'AuthController@login');  //Fitur API untuk Login
+Route::get('v1/user/profile', 'UserController@profile')->middleware('auth:api');
+Route::post('v1/place', 'PlaceController@add')->middleware('auth:api');
+Route::get('v1/place', 'PlaceController@show')->middleware('auth:api');
 
