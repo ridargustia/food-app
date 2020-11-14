dd6e8362719f93ef931246b761e8e75e95e4e2c1
Ridar Gustia <ridargp@gmail.com>
2020-11-14 13:25:49 +0700

    Menambahkan method craftsRandom dan travelsRandom di dalam file ProductController.php serta menambahkan routing di file route/api.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,11 +10,30 @@ class ProductController extends Controller
 {
     public function foodsRandom()
     {
-        // $foods = product::where('id_category', 1)->random(10);
-        $foods = product::where('id_category', 1)->get()->random(5);
+        $foods = product::where('id_category', 1)->get()->random(3);
 
         return fractal()
             ->collection($foods)
+            ->transformWith(new ProductTransformer)
+            ->toArray();
+    }
+
+    public function craftsRandom()
+    {
+        $crafts = product::where('id_category', 2)->get()->random(3);
+
+        return fractal()
+            ->collection($crafts)
+            ->transformWith(new ProductTransformer)
+            ->toArray();
+    }
+
+    public function travelsRandom()
+    {
+        $travels = product::where('id_category', 3)->get()->random(3);
+
+        return fractal()
+            ->collection($travels)
             ->transformWith(new ProductTransformer)
             ->toArray();
     }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ Route::post('v1/auth/register', 'AuthController@register');    //Fitur API untuk
 Route::post('v1/auth/login', 'AuthController@login');  //Fitur API untuk Login
 Route::get('v1/category', 'CategoryController@index');
 Route::get('v1/foods/random', 'ProductController@foodsRandom');
+Route::get('v1/crafts/random', 'ProductController@craftsRandom');
+Route::get('v1/travels/random', 'ProductController@travelsRandom');
 Route::get('v1/user/profile', 'UserController@profile')->middleware('auth:api');
 Route::post('v1/place', 'PlaceController@add')->middleware('auth:api');
 Route::get('v1/place', 'PlaceController@show')->middleware('auth:api');
